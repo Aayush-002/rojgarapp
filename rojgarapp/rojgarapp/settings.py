c092195133cefaee9f3be4917d845a9709476f50
adars124 <lonleyau@gmail.com>
2025-01-23 13:51:29 +0545

    feat: add deployment related files

--- rojgarapp/rojgarapp/settings.py
+++ rojgarapp/rojgarapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
 
 
 # Application definition
