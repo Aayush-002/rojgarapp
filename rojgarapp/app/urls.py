c16fc007ac648da3f8a8398bd6fdef26a12eb5ce
adars124 <lonleyau@gmail.com>
2025-04-09 01:39:32 +0545

    feat: add urls related to applications

--- rojgarapp/app/urls.py
+++ rojgarapp/app/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path("forms_list/", views.forms_list, name="forms_list"),
     path("<int:form_id>/edit/", views.forms_edit, name="forms_edit"),
     path("<int:form_id>/delete/", views.forms_delete, name="forms_delete"),
+    path('applications/', views.applications_list, name='application_list'),
+    path('applications/update-status/', views.update_application_status, name='update_application_status'),
+    path('applications/<int:pk>/delete/', views.application_delete, name='application_delete'),
     path("post-job/", views.post_job, name="post_job"),
+    path("job/<int:job_id>/", views.job_detail, name="job_detail"),
+    path("job/<int:job_id>/apply/", views.apply_job, name="apply_job"),
 ]
